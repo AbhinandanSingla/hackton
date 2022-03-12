49b178d4a865b2cc1e01e2c399f56a3d496f428d
Deepu Singla <abhinandansingla048@gmail.com>
2022-03-12 11:08:49 +0530

    init

---
+++ server/server.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyATqd6FkmtPisZ2cLzEyC2QPI6MFuuRUjE",
+    authDomain: "bteam-3165a.firebaseapp.com",
+    projectId: "bteam-3165a",
+    storageBucket: "bteam-3165a.appspot.com",
+    messagingSenderId: "204087729757",
+    appId: "1:204087729757:web:e80e9e9a96c79e9d6fb7b4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
